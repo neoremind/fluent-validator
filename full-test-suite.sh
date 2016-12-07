7b0bb3ffe13de2ce026901278741764ba9dbe3c5
Adam Berlin <berlin.ab@gmail.com>
2016-12-07 19:55:39 +0100

    Add fail-fast setting to the full test suite.

--- full-test-suite.sh
+++ full-test-suite.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e # fail script if anything fails.
+
 cd fluent-validator && mvn leanpackage jacoco:report
 cd fluent-validator-jsr303 && mvn clean package jacoco:report
 cd fluent-validator-spring && mvn clean package jacoco:report
