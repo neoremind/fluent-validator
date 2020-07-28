eee00cf7670159eb8f81737024ff51a80b231e1c
Elvys Soares <eas5@cin.ufpe.br>
2020-07-28 11:53:31 -0300

    Removed assertion roulette (test smell)

--- fluent-validator/src/test/java/com/baidu/unbiz/fluentvalidator/util/ClassUtilTest.java
+++ fluent-validator/src/test/java/com/baidu/unbiz/fluentvalidator/util/ClassUtilTest.java
@@ -10,29 +10,97 @@ import org.junit.Test;
 public class ClassUtilTest {
 
     @Test
-    public void getWrapperTypeIfPrimitive() {
+    public void getWrapperTypeIfPrimitive_int() {
         assertEquals(Integer.class, ClassUtil.getWrapperTypeIfPrimitive(int.class));
-        assertEquals(Long.class, ClassUtil.getWrapperTypeIfPrimitive(long.class));
-        assertEquals(Short.class, ClassUtil.getWrapperTypeIfPrimitive(short.class));
-        assertEquals(Double.class, ClassUtil.getWrapperTypeIfPrimitive(double.class));
-        assertEquals(Float.class, ClassUtil.getWrapperTypeIfPrimitive(float.class));
-        assertEquals(Character.class, ClassUtil.getWrapperTypeIfPrimitive(char.class));
-        assertEquals(Byte.class, ClassUtil.getWrapperTypeIfPrimitive(byte.class));
-        assertEquals(Boolean.class, ClassUtil.getWrapperTypeIfPrimitive(boolean.class));
-        assertEquals(Void.class, ClassUtil.getWrapperTypeIfPrimitive(void.class));
-
-        assertEquals(int[][].class, ClassUtil.getWrapperTypeIfPrimitive(int[][].class));
-        assertEquals(long[][].class, ClassUtil.getWrapperTypeIfPrimitive(long[][].class));
-        assertEquals(short[][].class, ClassUtil.getWrapperTypeIfPrimitive(short[][].class));
-        assertEquals(double[][].class, ClassUtil.getWrapperTypeIfPrimitive(double[][].class));
-        assertEquals(float[][].class, ClassUtil.getWrapperTypeIfPrimitive(float[][].class));
-        assertEquals(char[][].class, ClassUtil.getWrapperTypeIfPrimitive(char[][].class));
-        assertEquals(byte[][].class, ClassUtil.getWrapperTypeIfPrimitive(byte[][].class));
-        assertEquals(boolean[][].class, ClassUtil.getWrapperTypeIfPrimitive(boolean[][].class));
-
-        assertEquals(String.class, ClassUtil.getWrapperTypeIfPrimitive(String.class));
-
-        assertEquals(String[][].class, ClassUtil.getWrapperTypeIfPrimitive(String[][].class));
     }
 
+    @Test
+    public void getWrapperTypeIfPrimitive_long() {
+        assertEquals(Long.class, ClassUtil.getWrapperTypeIfPrimitive(long.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_short() {
+        assertEquals(Short.class, ClassUtil.getWrapperTypeIfPrimitive(short.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_double() {
+        assertEquals(Double.class, ClassUtil.getWrapperTypeIfPrimitive(double.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_float() {
+        assertEquals(Float.class, ClassUtil.getWrapperTypeIfPrimitive(float.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_char() {
+        assertEquals(Character.class, ClassUtil.getWrapperTypeIfPrimitive(char.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_byte() {
+        assertEquals(Byte.class, ClassUtil.getWrapperTypeIfPrimitive(byte.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_boolean() {
+        assertEquals(Boolean.class, ClassUtil.getWrapperTypeIfPrimitive(boolean.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_void() {
+        assertEquals(Void.class, ClassUtil.getWrapperTypeIfPrimitive(void.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_String() {
+        assertEquals(String.class, ClassUtil.getWrapperTypeIfPrimitive(String.class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_int2dArray() {
+        assertEquals(int[][].class, ClassUtil.getWrapperTypeIfPrimitive(int[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_long2dArray() {
+        assertEquals(long[][].class, ClassUtil.getWrapperTypeIfPrimitive(long[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_short2dArray() {
+        assertEquals(short[][].class, ClassUtil.getWrapperTypeIfPrimitive(short[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_double2dArray() {
+        assertEquals(double[][].class, ClassUtil.getWrapperTypeIfPrimitive(double[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_float2dArray() {
+        assertEquals(float[][].class, ClassUtil.getWrapperTypeIfPrimitive(float[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_char2dArray() {
+        assertEquals(char[][].class, ClassUtil.getWrapperTypeIfPrimitive(char[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_byte2dArray() {
+        assertEquals(byte[][].class, ClassUtil.getWrapperTypeIfPrimitive(byte[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_boolean2dArray() {
+        assertEquals(boolean[][].class, ClassUtil.getWrapperTypeIfPrimitive(boolean[][].class));
+    }
+
+    @Test
+    public void getWrapperTypeIfPrimitive_String2dArray() {
+        assertEquals(String[][].class, ClassUtil.getWrapperTypeIfPrimitive(String[][].class));
+    }
 }
